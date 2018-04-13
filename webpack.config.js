950d301c1fb8bb19ae5e0ba54f2907e7ec461409
Felipe Sales <sales.felipeb@gmail.com>
2018-04-13 11:19:14 -0300

    Fix Babel and webpack configs to publish on npm

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path')
 module.exports = {
   entry: './src/BuyButton/index.js',
   output: {
-    path: path.resolve(__dirname, 'build'),
+    path: path.resolve(__dirname, 'lib'),
     filename: 'index.js',
     libraryTarget: 'commonjs2',
   },
@@ -12,12 +12,9 @@ module.exports = {
       {
         test: /\.js$/,
         include: path.resolve(__dirname, 'src'),
-        exclude: /(node_modules|bower_components|build)/,
+        exclude: /(node_modules|bower_components|lib)/,
         use: {
           loader: 'babel-loader',
-          options: {
-            presets: ['env'],
-          },
         },
       },
 
