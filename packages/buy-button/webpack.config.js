a54a990c4a3b6fa9f0e9dd4ceda7cd578febec69
Lucas Cordeiro <ecdb.lucas@gmail.com>
2018-04-17 10:57:01 -0300

    Move source files to src folder

--- packages/buy-button/webpack.config.js
+++ packages/buy-button/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 
 module.exports = {
-  entry: './src/BuyButton/index.js',
+  entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'lib'),
     filename: 'index.js',
