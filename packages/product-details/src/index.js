7bb1da8b261916ee1cf90ab5065dea847a2701a1
Lucas Cordeiro <ecdb.lucas@gmail.com>
2018-04-25 14:10:54 -0300

    Add discount badge component

--- packages/product-details/src/index.js
+++ packages/product-details/src/index.js
@@ -1,3 +1,4 @@
 export { default as Price } from './Price'
 export { default as ProductName } from './ProductName'
 export { default as QuantitySelector } from './QuantitySelector'
+export { default as DiscountBadge } from './DiscountBadge'
