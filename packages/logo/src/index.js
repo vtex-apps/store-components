d9395207ee3e9730905fd253c2f6fa372511f3e7
Lucas Cordeiro <ecdb.lucas@gmail.com>
2018-05-02 11:29:29 -0300

    Update export and change proptypes to be static

--- packages/logo/src/index.js
+++ packages/logo/src/index.js
@@ -4,19 +4,17 @@ import PropTypes from 'prop-types'
 /**
  * Logo of the store
  */
-class Logo extends Component {
+export default class Logo extends Component {
+  static propTypes = {
+    /** URL of the logo */
+    url: PropTypes.string.isRequired,
+    /** Title to be displayed as alt text */
+    title: PropTypes.string.isRequired,
+  }
+
   render() {
     const { url, title } = this.props
 
     return <img className="vtex-logo" src={url} alt={title} />
   }
 }
-
-Logo.propTypes = {
-  /** URL of the logo */
-  url: PropTypes.string.isRequired,
-  /** Title to be displayed as alt text */
-  title: PropTypes.string.isRequired,
-}
-
-export default Logo
