f518952279d1dcf108bc4af652f4198003ed8c52
gugabribeiro <gustavo.ribeiro@ccc.ufcg.edu.br>
2018-05-04 11:35:30 -0300

    Add internationalization

--- packages/product-description/src/ProductDescription.js
+++ packages/product-description/src/ProductDescription.js
@@ -1,20 +1,11 @@
-import React, { Component } from 'react'
+import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
-import { injectIntl, intlShape, addLocaleData } from 'react-intl'
+import { injectIntl, intlShape } from 'react-intl'
 
 import VTEXClasses from './CustomClasses'
 
-import ptLocale from './locales/pt-BR.json'
-import enLocale from './locales/en-US.json'
-
-addLocaleData([
-  { ...ptLocale, locale: 'pt-BR' },
-  { ...enLocale, locale: 'en-US' },
-])
-
-class ProductDescription extends Component {
+class ProductDescription extends PureComponent {
   render() {
-    console.log(this.props.intl)
     return (
       <div className={`${VTEXClasses.PRODUCT_DESCRIPTION} ma2`}>
         <div className="f4 b ttu mb3">
