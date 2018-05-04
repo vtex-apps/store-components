3404195b39899112c77e400e57d436f983f060d7
Felipe Sales <sales.felipeb@gmail.com>
2018-05-04 10:38:33 -0300

    Init npm component

---
+++ packages/search-bar/src/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import Input from '@vtex/styleguide/lib/Input'
+
+export default class SearchBar extends Component {
+  render() {
+    return <Input size="large" label="Large" />
+  }
+}
+
+SearchBar.propTypes = {}
