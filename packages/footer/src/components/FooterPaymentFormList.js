3c8084528b3655a7c30619a37c77dd1a6217d048
Lucas Cordeiro <ecdb.lucas@gmail.com>
2018-05-07 12:00:30 -0300

    Add payment form images

--- packages/footer/src/components/FooterPaymentFormList.js
+++ packages/footer/src/components/FooterPaymentFormList.js
@@ -1,13 +1,34 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import footerList from './footerList'
 
-const FooterPaymentFormItem = ({ paymentType }) => <span>{paymentType}</span>
+class FooterPaymentFormItem extends Component {
+  state = {}
 
+  componentDidMount() {
+    const { paymentType, showInColor } = this.props
+
+    import(`../images/${paymentType}${showInColor ? '' : '-BW'}.svg`)
+      .then(image => {
+        this.setState({ image })
+      })
+  }
+
+  render() {
+    const { image } = this.state
+
+    if (!image) {
+      return null
+    }
+
+    return <span className="vtex-footer__payment-form-item" dangerouslySetInnerHTML={{ __html: image }} />
+  }
+}
 FooterPaymentFormItem.displayName = 'FooterPaymentFormItem'
 
 FooterPaymentFormItem.propTypes = {
   paymentType: PropTypes.string.isRequired,
+  showInColor: PropTypes.bool.isRequired,
 }
 
 export default footerList(FooterPaymentFormItem)
