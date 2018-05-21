683ca15dee2fbdda76d35607289dc634dd7f11f2
Felipe Sales <sales.felipeb@gmail.com>
2018-05-21 11:40:28 -0300

    Create files

---
+++ react/components/Breadcrumbs/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+/**
+ * Breadcrumb Component.
+ */
+export class Breadcrumb extends Component {
+  render() {
+    return <div>Breadcrumb</div>
+  }
+}
+
+Breadcrumb.propTypes = {}
+
+export default Breadcrumb
