5c34db36c03896c2674606abd7e0681e28342809
Felipe Sales <sales.felipeb@gmail.com>
2018-05-28 08:28:23 -0300

    Remove console log

--- react/components/ProductDescription/index.js
+++ react/components/ProductDescription/index.js
@@ -6,7 +6,6 @@ import VTEXClasses from './CustomClasses'
 
 import './global.css'
 
-
 /**
  * Product Description Component.
  * Render the description of a product and it's technical specifications.
@@ -15,14 +14,12 @@ class ProductDescription extends Component {
   render() {
     const { specifications, skuName } = this.props
 
-    console.log(specifications, skuName)
-
     return (
       <div className={`${VTEXClasses.PRODUCT_DESCRIPTION} ma2`}>
         <div className="f4 b ttu mb3">
           <FormattedMessage id="product-description.title" />
         </div>
-        
+
         {this.props.children}
 
         {specifications.length && (
@@ -41,14 +38,18 @@ class ProductDescription extends Component {
                   </tr>
                 )}
                 {specifications.map(specification => (
-                  <tr key={specification.name} className="vtex-product-specifications__table-row">
+                  <tr
+                    key={specification.name}
+                    className="vtex-product-specifications__table-row"
+                  >
                     <th className="vtex-product-specifications__specification-name">
                       {specification.name}
                     </th>
                     <td className="vtex-product-specifications__specification-values">
                       {specification.values.map((value, i) => (
                         <Fragment key={value}>
-                          {value} {i !== specification.values.length - 1 && <br />}
+                          {value}{' '}
+                          {i !== specification.values.length - 1 && <br />}
                         </Fragment>
                       ))}
                     </td>
@@ -74,10 +75,12 @@ ProductDescription.propTypes = {
   /** Intl object to provides internationalization */
   intl: intlShape.isRequired,
   /** Specifications of the product */
-  specifications: PropTypes.arrayOf(PropTypes.shape({
-    name: PropTypes.string.isRequired,
-    values: PropTypes.arrayOf(PropTypes.string).isRequired,
-  })),
+  specifications: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      values: PropTypes.arrayOf(PropTypes.string).isRequired,
+    })
+  ),
   /** Name of the current SKU */
   skuName: PropTypes.string,
 }
