7cc741d7c5f6123753cfd2ba0f4f06b47f7898c5
Felipe Sales <sales.felipeb@gmail.com>
2018-05-29 14:41:33 -0300

    Add optional attibute for brand and product SKU

--- react/components/ProductName/index.js
+++ react/components/ProductName/index.js
@@ -12,25 +12,40 @@ class ProductName extends Component {
     name: PropTypes.string.isRequired,
     /** Selected SKU name */
     skuName: PropTypes.string,
+    /** Show SKU name */
+    showSkuName: PropTypes.bool,
     /** Brand name */
     brandName: PropTypes.string,
+    /** Show brand name */
+    showBrandName: PropTypes.bool,
     /** Display large font */
     large: PropTypes.bool,
   }
 
   static defaultProps = {
     large: false,
+    showBrandName: true,
+    showSkuName: true,
   }
 
   render() {
-    const { name, skuName, brandName, large } = this.props
+    const { name, skuName, brandName, large, showBrandName, showSkuName } = this.props
 
+    const hiden = 'vtex-product-name--hiden'
     let brandClasses = 'vtex-product-name__brand'
     let skuClasses = 'vtex-product-name__sku'
 
     if (large) {
       brandClasses += ' vtex-product-name__brand--large'
       skuClasses += ' vtex-product-name__sku--large'
+    }
+
+    if (!showBrandName) {
+      brandClasses += ` ${hiden}`
+    }
+
+    if (!showSkuName) {
+      skuClasses += ` ${hiden}`
     }
 
     return (
