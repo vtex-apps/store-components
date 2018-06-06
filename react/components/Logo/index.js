3aeeefed1ed934de6ce37173a5073e11d175c95c
gugabribeiro <gustavo.ribeiro@ccc.ufcg.edu.br>
2018-06-06 13:53:40 -0300

    Add defaultlogo as svg file inside component

--- react/components/Logo/index.js
+++ react/components/Logo/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
+import DefaultLogo from './images/DefaultLogo.svg'
+
 import './global.css'
 
 /**
@@ -9,18 +11,17 @@ import './global.css'
 export default class Logo extends Component {
   static propTypes = {
     /** URL of the logo */
-    url: PropTypes.string.isRequired,
+    url: PropTypes.string,
     /** Title to be displayed as alt text */
     title: PropTypes.string.isRequired,
   }
 
   static defaultProps = {
-    url: 'http://brand.vtex.com/static/media/logo.2f3fc60b.svg',
     title: 'VTEX logo',
   }
   
   render() {
     const { url, title } = this.props
-    return <img className="vtex-logo" src={url} alt={title} />
+    return <img className="vtex-logo" src={url || DefaultLogo} alt={title} />
   }
 }
