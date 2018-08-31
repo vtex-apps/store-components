8eb8ec50ad812d5ed20b3ea376535b9f20fd2ea3
Vitória Heliane <vitoriaheliane@gmail.com>
2018-08-31 11:40:51 -0300

    Fix minicart label color

--- react/components/Header/components/TopMenu.js
+++ react/components/Header/components/TopMenu.js
@@ -68,6 +68,7 @@ class TopMenu extends Component {
           iconColor="#979899"
           iconSize={mobileMode ? MINICART_ICON_SIZE_MOBILE : MINICART_ICON_SIZE_DESKTOP}
           iconLabel={!mobileMode ? this.translate('topMenu.minicart.icon.label') : ''}
+          labelClasses="gray"
         />
       </div>
     )
