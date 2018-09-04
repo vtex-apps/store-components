3657ddef9ca3d018353fb1229976eb0684ebb570
Vitória Heliane <vitoriaheliane@gmail.com>
2018-09-04 09:03:57 -0300

    Add the Animation component implementation

--- react/components/Animation/index.js
+++ react/components/Animation/index.js
@@ -1,9 +1,62 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+import { Transition } from 'react-spring'
+
+const ANIMATIONS = {
+  'left': {
+    from: { transform: 'translateX(100%)' },
+    enter: { transform: 'translateX(0%)' },
+    leave: { transform: 'translateX(100%)' },
+  },
+  'right': {
+    from: { transform: 'translateX(-100%)' },
+    enter: { transform: 'translateX(0%)' },
+    leave: { transform: 'translateX(-100%)' },
+  },
+  'top': {
+    from: { transform: 'translateY(-100%)' },
+    enter: { transform: 'translateY(0%)' },
+    leave: { transform: 'translateY(-100%)' },
+  },
+  'bottom': {
+    from: { transform: 'translateY(100%)' },
+    enter: { transform: 'translateY(0%)' },
+    leave: { transform: 'translateY(100%)' },
+  },
+}
 
 export default class Animation extends Component {
+  static propTypes = {
+    children: PropTypes.object.isRequired,
+    isShown: PropTypes.bool,
+    className: PropTypes.string,
+    from: PropTypes.oneOf(['left', 'right', 'top', 'bottom']),
+  }
+
+  static defaultProps = {
+    className: '',
+    from: 'top',
+  }
+
+  renderChildren = style => (
+    <div className={this.props.className}
+      style={style}>
+      {this.props.children}
+    </div>
+  )
+
   render() {
+    const { isShown, from } = this.props
+    const style = ANIMATIONS[from]
+
     return (
-      <div></div>
+      <Transition
+        keys={isShown ? ['children'] : []}
+        {...style}
+      >
+        {isShown ? [this.renderChildren] : []}
+      </Transition>
     )
   }
 }
