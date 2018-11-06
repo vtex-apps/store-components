20900cb17e37b9153d7436420e2b0d2dd0c5991c
Dan <daniel.ddsc@gmail.com>
2018-11-06 11:22:30 -0200

    Port Greeting component from 'delivery-dreamstore'

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,8 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
+### Added
+- `Greeting` component that renders a welcome message with the user first name
 
 ## [2.4.3] - 2018-10-31
 ### Fixed
---
+++ react/components/Greeting/Loader.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import ContentLoader from 'react-content-loader'
+
+const GreetingLoading = props => (
+  <ContentLoader
+    height={32}
+    width={300}
+    speed={2}
+    primaryColor="#f3f3f3"
+    secondaryColor="#ecebeb"
+    className="v-mid"
+    {...props}
+  >
+    <rect rx="1" ry="1" width="300" height="32" />
+  </ContentLoader>
+)
+
+export default GreetingLoading
