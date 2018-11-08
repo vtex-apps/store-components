25da6d70f59d6beb414b50f41ac5b769c9a5d3c4
Luciano Júnior <luciannojunior@gmail.com>
2018-11-07 21:35:39 -0300

    Initial commit˜

--- react/components/ProductPrice/index.js
+++ react/components/ProductPrice/index.js
@@ -22,6 +22,7 @@ class Price extends Component {
     showLabels: true,
     showInstallments: false,
     showSavings: false,
+    labelSellingPrice: null
   }
 
   currencyOptions = {
@@ -39,6 +40,7 @@ class Price extends Component {
       showInstallments,
       showLabels,
       showSavings,
+      labelSellingPrice,
       installments,
       styles,
       intl: { formatNumber },
@@ -67,7 +69,7 @@ class Price extends Component {
         <div className="vtex-price-selling__container pv1 b c-muted-1">
           {showLabels && (
             <div className="vtex-price-selling__label dib">
-              <FormattedMessage id="pricing.to" />
+              {labelSellingPrice || <FormattedMessage id="pricing.to" />}
             </div>
           )}
           <div className="vtex-price-selling dib ph2">
@@ -170,6 +172,11 @@ priceWithIntel.schema = {
       default: Price.defaultProps.showSavings,
       isLayout: true,
     },
+    labelSellingPrice: {
+      type: 'string',
+      title: 'editor.productPrice.labelSellingPrice',
+      default: Price.defaultProps.labelSellingPrice,
+    }
   },
 }
 
