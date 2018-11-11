29c674c7f88bd8bf15a266f1e7b3ad240da83c12
Felipe Sales <sales.felipeb@gmail.com>
2018-11-11 15:23:58 -0200

    Adding error pro to GenericError

--- react/components/Account/components/GenericError.js
+++ react/components/Account/components/GenericError.js
@@ -3,18 +3,19 @@ import PropTypes from 'prop-types'
 import { intlShape, injectIntl } from 'react-intl'
 import { Alert } from 'vtex.styleguide'
 
-const GenericError = ({ errorId, onDismiss, intl }) => {
+const GenericError = ({ error, errorId, onDismiss, intl }) => {
   return (
     <div className="mb5">
       <Alert type="error" onClose={onDismiss}>
-        {intl.formatMessage({ id: errorId })}
+        {errorId ? intl.formatMessage({ id: errorId }) : error}
       </Alert>
     </div>
   )
 }
 
 GenericError.propTypes = {
-  errorId: PropTypes.string.isRequired,
+  error: PropTypes.string,
+  errorId: PropTypes.string,
   onDismiss: PropTypes.func,
   intl: intlShape.isRequired,
 }
