743c41fdccb3f43123deee5ca4464ef1878a296e
Felipe Sales <sales.felipeb@gmail.com>
2018-11-13 10:54:58 -0200

    Removing Account bundle

--- react/components/Account/index.js
+++
@@ -1,5 +0,0 @@
-import BaseLoading from './components/BaseLoading'
-import ContentWrapper from './components/ContentWrapper'
-import GenericError from './components/GenericError'
-
-export default { BaseLoading, ContentWrapper, GenericError }
