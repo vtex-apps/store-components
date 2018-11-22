6d3c91c437dbae8d9e71bed2650ad8bdda73e7b1
Rerisson Daniel <rerisson.matos@splab.ufcg.edu.br>
2018-11-22 15:09:44 -0300

    Replace html table by StyleGuide table

--- react/components/ProductDescription/index.js
+++ react/components/ProductDescription/index.js
@@ -1,6 +1,8 @@
 import React, { Component, Fragment } from 'react'
 import PropTypes from 'prop-types'
 import { FormattedMessage, injectIntl, intlShape } from 'react-intl'
+import { Table } from 'vtex.styleguide'
+import classNames from 'classnames'
 
 import SpecificationRow from './SpecificationRow'
 
@@ -11,12 +13,45 @@ import './global.css'
  * Render the description and technical specifications of a product
  */
 class ProductDescription extends Component {
+
+  static specificationSchema = {
+    'properties': {
+      'name': {
+        'type': 'string',
+        'title': 'Name',
+        cellRenderer: ({ cellData: name }) => {
+          return (
+            <span className="vtex-product-specifications__specification-name dtc-ns tr-ns w-20-ns pb2-ns ttu t-body  c-muted-1 pt2  ph6 br-ns b--muted-3">
+              {name}
+            </span>
+          )
+        }
+      },
+      'values': {
+        'type': "array",
+        'title': 'Values',
+        'items': {
+          'type': 'string',
+          'title': 'Items',
+        },
+        cellRenderer: ({ cellData: value }) => {
+          return (
+            <span className="vtex-product-specifications__specification-values dtc-ns pv2 ph6 c-muted-2 db-s">
+              {value}
+            </span>
+          )
+        }
+      }
+    }
+  }
+
   render() {
     const { specifications, skuName, description } = this.props
 
     if (!description || !specifications) {
       return null
     }
+
 
     return (
       <div className="vtex-product-description ma2">
@@ -27,6 +62,17 @@ class ProductDescription extends Component {
           className="vtex-product-description__text measure-wide c-muted-1"
           dangerouslySetInnerHTML={{ __html: description }}
         />
+
+        {specifications.length > 0 && (
+          <div className="vtex-product-specifications mt6">
+            <div className="vtex-product-specifications__title t-heading-5 mb6-ns mb5-s">
+              <FormattedMessage id="technicalspecifications.title" />
+            </div>
+            <div className="vtex-product-specifications__table w-100">
+              <Table items={specifications} schema={ProductDescription.specificationSchema} density={'high'} disableHeader />
+            </div>
+          </div>
+        )}
 
         {specifications.length > 0 && (
           <div className="vtex-product-specifications mt6">
