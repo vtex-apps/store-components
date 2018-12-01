0c5922416a69f2a6fe4e68afe49445a4dc237322
SamuraiExx <samuraiexx@gmail.com>
2018-12-01 19:32:09 -0200

    Update video components propTypes and refactor

--- react/components/ProductImages/components/Video/Vimeo.js
+++ react/components/ProductImages/components/Video/Vimeo.js
@@ -2,33 +2,37 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
 class Vimeo extends Component {
-  static getThumbUrl = (url, thumbWidth) => new Promise((resolve, reject) => {
-    const getUrl = `https://vimeo.com/api/oembed.json?url=${url}`
-    fetch(getUrl)
-      .then(response => {
-        return response.json()
-      })
-      .then((resp) => {
-        resolve(Vimeo.thumbUrlFromResp(resp, thumbWidth))
-      })
-  })
+  static getThumbUrl = (url, thumbWidth) =>
+    new Promise(resolve => {
+      const getUrl = `https://vimeo.com/api/oembed.json?url=${url}`
+      fetch(getUrl)
+        .then(response => {
+          return response.json()
+        })
+        .then(response => {
+          resolve(Vimeo.thumbUrlFromResp(response, thumbWidth))
+        })
+    });
 
-  static thumbUrlFromResp(resp, thumbWidth) {
-    const {height, width} = resp
-    const thumb = resp.thumbnail_url_with_play_button
+  static thumbUrlFromResp(response, thumbWidth) {
+    const { height, width } = response
+    const thumb = response.thumbnail_url_with_play_button
 
-    thumbWidth = thumbWidth || resp.thumbnail_width
-    const thumbHeight = Math.ceil(thumbWidth*height/width)
+    thumbWidth = thumbWidth || response.thumbnail_width
+    const thumbHeight = Math.ceil((thumbWidth * height) / width)
 
-    return thumb.replace(/_[123456789]*x[123456789]*./, `_${thumbWidth}x${thumbHeight}.`)
+    return thumb.replace(
+      /_[123456789]*x[123456789]*./,
+      `_${thumbWidth}x${thumbHeight}.`
+    )
   }
 
   constructor(props) {
     super(props)
 
-    this.state = {iframe: {}}
+    this.state = { iframe: {} }
 
-    const {loop, autoplay, width, height, showTitle, url} = this.props
+    const { loop, autoplay, width, height, showTitle, url } = this.props
     const params = `autoplay=${autoplay}&loop=${loop}&title=${showTitle}&width=${width}&height=${height}`
     const getUrl = `https://vimeo.com/api/oembed.json?url=${url}&${params}`
 
@@ -38,54 +42,64 @@ class Vimeo extends Component {
       .then(response => {
         return response.json()
       })
-      .then((resp) => {
-        const {height, width, html, title } = resp
+      .then(response => {
+        const { height, width, html, title } = response
 
-        const thumbUrl = Vimeo.thumbUrlFromResp(resp, props.thumbWidth)
+        const thumbUrl = Vimeo.thumbUrlFromResp(response, props.thumbWidth)
         props.setThumb && props.setThumb(thumbUrl, title)
 
-        const src = html.match(/src= *" *([^"]*) *"/)[1] //Get url from response
+        const src = html.match(/src= *" *([^"]*) *"/)[1] // Get url from response
 
-        this.setState({ iframe: { divStyle: {padding: `${100 * height / width}% 0 0 0`}, src: src }})
+        this.setState({
+          iframe: {
+            divStyle: { padding: `${(100 * height) / width}% 0 0 0` },
+            src: src,
+          },
+        })
       })
   }
 
-  componentDidMount(){
-    this.iframeRef.onload = () => this.frameReady = true
+  componentDidMount() {
+    this.iframeRef.onload = () => (this.frameReady = true)
   }
 
-  executeCommand = (command) => () => {
-    if(!this.frameReady) return
+  executeCommand = command => () => {
+    if (!this.frameReady) return
 
-    const vimeo_command = JSON.stringify( { method: command } )
-    this.iframeRef.contentWindow.postMessage(vimeo_command, 'https://player.vimeo.com')
-  }
+    const vimeoCommand = JSON.stringify({ method: command })
+    this.iframeRef.contentWindow.postMessage(
+      vimeoCommand,
+      'https://player.vimeo.com'
+    )
+  };
 
   render() {
     const { iframe } = this.state
     const { className, id } = this.props
 
-    this.props.playing ?
-      this.executeCommand('play')() :
-      this.executeCommand('pause')()
+    this.props.playing
+      ? this.executeCommand('play')()
+      : this.executeCommand('pause')()
 
-    return <div style={iframe.divStyle} className={`relative ${className}`}>
-      <iframe
-        ref={this.iframeRef}
-        title={id}
-        className="absolute top-0 left-0 w-100 h-100"
-        src={iframe.src}
-        frameBorder="0"
-        allowFullScreen
-        allow="autoplay">
-      </iframe>
-    </div>
+    return (
+      <div style={iframe.divStyle} className={`relative ${className}`}>
+        <iframe
+          ref={this.iframeRef}
+          title={id}
+          className="absolute top-0 left-0 w-100 h-100"
+          src={iframe.src}
+          frameBorder="0"
+          allowFullScreen
+          allow="autoplay"
+        />
+      </div>
+    )
   }
 }
 
-Vimeo.propsTypes = {
+Vimeo.propTypes = {
   url: PropTypes.string.isRequired,
-  id: PropTypes.number.isRequired, //Unique ID for iframe title
+  id: PropTypes.number.isRequired, // Unique ID for iframe title
   setThumb: PropTypes.func,
   thumbWidth: PropTypes.number,
   className: PropTypes.string,
