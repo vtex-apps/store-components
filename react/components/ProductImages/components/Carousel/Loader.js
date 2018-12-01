39279d1dce3b585da3e489ca713a4d9c05b4fbf3
SamuraiExx <samuraiexx@gmail.com>
2018-12-01 19:35:47 -0200

    Update formatting and add propTypes

--- react/components/ProductImages/components/Carousel/Loader.js
+++ react/components/ProductImages/components/Carousel/Loader.js
@@ -1,23 +1,24 @@
 import ContentLoader from 'react-content-loader'
+import React from 'react'
 
-export default (props) => {
-  if(props.slidesAmount === 1)
+export default props => {
+  if (props.slidesAmount === 1) {
     return (
       <div className="w-100 aspect-ratio aspect-ratio--1x1">
         <ContentLoader
           style={{
-          position: 'absolute',
-          width: '100%',
-          height: '100%',
-        }}
+            position: 'absolute',
+            width: '100%',
+            height: '100%',
+          }}
           height="100%"
-          width="100%">
-          <rect
-            width="100%"
-            height="100%"
-          />
+          width="100%"
+        >
+          <rect width="100%" height="100%" />
         </ContentLoader>
-      </div>)
+      </div>
+    )
+  }
 
   return (
     <div className="w-100 aspect-ratio aspect-ratio--1x1">
@@ -28,41 +29,14 @@ export default (props) => {
           height: '100%',
         }}
         height="100%"
-        width="100%">
-        <rect
-          className="dn db-ns"
-          width="20%"
-          height="20%"
-        />
-        <rect
-          className="dn db-ns"
-          width="20%"
-          height="20%"
-          y="21%"
-        />
-        <rect
-          className="dn db-ns"
-          width="20%"
-          height="20%"
-          y="42%"
-        />
-        <rect
-          className="dn db-ns"
-          width="20%"
-          height="20%"
-          y="63%"
-        />
-        <rect
-          className="dn db-ns"
-          width="89%"
-          height="89%"
-          x="21%"
-        />
-        <rect
-          className="db dn-ns"
-          width="100%"
-          height="100%"
-        />
+        width="100%"
+      >
+        <rect className="dn db-ns" width="20%" height="20%" />
+        <rect className="dn db-ns" width="20%" height="20%" y="21%" />
+        <rect className="dn db-ns" width="20%" height="20%" y="42%" />
+        <rect className="dn db-ns" width="20%" height="20%" y="63%" />
+        <rect className="dn db-ns" width="89%" height="89%" x="21%" />
+        <rect className="db dn-ns" width="100%" height="100%" />
       </ContentLoader>
     </div>
   )
