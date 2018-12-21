b042f933914f0e23938998d062806f6952786614
Juan Almeida <juan.satyro@gmail.com>
2018-12-21 16:06:01 -0200

    Fix import

--- react/components/CollectionBadges/components/CollectionBadgeItem.js
+++ react/components/CollectionBadges/components/CollectionBadgeItem.js
@@ -1,7 +1,7 @@
 import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
 
-import collectionBadges from './collectionBadges.css'
+import collectionBadges from '../collectionBadges.css'
 
 /**
  * Collection Badge Item.
