3b5c837ebd925e314a8ee65575b6a3ec62f6c24d
Lucas Cordeiro <ecdb.lucas@gmail.com>
2019-01-09 17:00:05 -0300

    Proxy unused props of Container

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,8 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
+### Changed
+- Proxy unused props of the `Container` component to the root element.
 
 ## [3.5.1] - 2019-01-09
 ### Added
--- react/components/Container/index.js
+++ react/components/Container/index.js
@@ -2,11 +2,11 @@ import classNames from 'classnames'
 import PropTypes from 'prop-types'
 import React from 'react'
 
-const Container = ({ className, children }, ref) => {
+const Container = ({ className, children, ...props }, ref) => {
   const classes = classNames('ph3 ph5-m ph8-l ph9-xl', className)
 
   return (
-    <section className={classes} ref={ref}>
+    <section {...props} className={classes} ref={ref}>
       {children}
     </section>
   )
