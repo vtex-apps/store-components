693f0a9e5f6b5e58abbe8648370ec2d2b1758276
Juan Almeida <juan.satyro@gmail.com>
2019-02-18 15:02:20 -0300

    Add base settings for tests

--- react/setupTests.js
+++ react/setupTests.js
@@ -1,23 +1,14 @@
-import { configure } from 'enzyme'
+import Enzyme from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
-import { JSDOM } from 'jsdom'
 
-import loadLocales from './__tests__/helpers/loadLocales'
-loadLocales()
+Enzyme.configure({ adapter: new Adapter() })
 
-const jsdom = new JSDOM('<!doctype html><html><body></body></html>')
-
-global.window = jsdom.window
-global.document = window.document
-
-configure({ adapter: new Adapter() })
-
-global.window.matchMedia =
-  global.window.matchMedia ||
-  function() {
+window.matchMedia =
+  window.matchMedia ||
+  function () {
     return {
       matches: false,
-      addListener: function() {},
-      removeListener: function() {},
+      addListener: function () { },
+      removeListener: function () { },
     }
   }
