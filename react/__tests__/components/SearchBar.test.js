f01e4eaaa5d566e4d8267f7150b8e3da162f6957
Theo Moura <theomouram@gmail.com>
2019-02-21 15:36:09 -0300

    Creating a mock provider

--- react/__tests__/components/SearchBar.test.js
+++ react/__tests__/components/SearchBar.test.js
@@ -1,63 +1,34 @@
 import React from 'react'
 import { MockedProvider } from 'react-apollo/test-utils'
-import { mount } from 'enzyme'
-import { IntlProvider } from 'react-intl'
+import { renderWithIntl } from 'intl-helper'
+import { createClientMock } from '../../testUtils/mockProvider'
 
-import autocomplete from '../../components/SearchBar/queries/autocomplete.gql'
 import SearchBar from '../../SearchBar'
 
 describe('<SearchBar />', () => {
-  let wrapper
+  const mockedResult = {
+    data: {
+      loading: false,
+      autocomplete: {
+        itemsReturned: [{ thumb: 'thumb', name: 'name', href: 'href' }],
+      },
+    },
+  }
 
-  beforeEach(done => {
-    wrapper = mount(
-      <MockedProvider
-        mocks={[
-          {
-            request: {
-              query: autocomplete,
-              variables: {
-                maxRows: 2,
-                searchTerm: 'shirt',
-              },
-            },
-            result: {
-              data: {
-                loading: false,
-                autocomplete: {
-                  itemsReturned: [
-                    { thumb: 'thumb', name: 'name', href: 'href' },
-                  ],
-                },
-              },
-            },
-          },
-        ]}
-      >
-        <IntlProvider
-          messages={{
-            'search.placeholder': 'Search here',
-            'search.noMatches': 'Nothing was found',
-          }}
-          locale="en"
-        >
-          <SearchBar />
-        </IntlProvider>
+  const renderComponent = (customProps = {}) => {
+    const client = createClientMock(mockedResult)
+    return renderWithIntl(
+      <MockedProvider client={client}>
+        <SearchBar />
       </MockedProvider>
     )
-
-    process.nextTick(() => {
-      wrapper.update()
-      done()
-      return wrapper
-    })
-  })
+  }
 
   it('should be able to mount and not break', () => {
-    expect(wrapper).toBeTruthy()
-  }, 10000)
+    expect(renderComponent({ hasIconLeft: true })).toBeTruthy()
+  })
 
   it('should match snapshot', () => {
-    expect(wrapper).toMatchSnapshot()
+    expect(renderComponent({ hasIconLeft: true })).toMatchSnapshot()
   })
 })
