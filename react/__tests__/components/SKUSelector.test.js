3d005af386581ff703007fd5b6d4e4cf2588561a
Juan Almeida <juan.satyro@gmail.com>
2019-02-21 15:36:14 -0300

    Fix tests

--- react/__tests__/components/SKUSelector.test.js
+++ react/__tests__/components/SKUSelector.test.js
@@ -2,13 +2,13 @@ import React from 'react'
 import { renderWithIntl } from 'intl-helper'
 
 import SKUSelector from './../../SKUSelector'
-import SKU from 'sku-helper'
+import { getSKU } from 'sku-helper'
 
 describe('<SKUSelector />', () => {
   const renderComponent = customProps => {
     const props = {
-      skuSelected: SKU.getSKU(),
-      skuItems: [SKU.getSKU('Black'), SKU.getSKU('Blue'), SKU.getSKU('Yellow')],
+      skuSelected: getSKU(),
+      skuItems: [getSKU('Black'), getSKU('Blue'), getSKU('Yellow')],
       ...customProps,
     }
     return renderWithIntl(<SKUSelector {...props} />)
