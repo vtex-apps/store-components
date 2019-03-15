cfb0fc7abb7083eaccf1a2d6fca0e9f16f9a55ca
Juan Almeida <juan.satyro@gmail.com>
2019-03-15 17:39:21 -0300

    Fixing SKUSelector PropTypes

--- react/components/SKUSelector/utils/proptypes.js
+++ react/components/SKUSelector/utils/proptypes.js
@@ -1,47 +1,64 @@
 import PropTypes from 'prop-types'
 
-const skuPropType = PropTypes.shape({
-  /** Name of the SKU Item */
-  name: PropTypes.string.isRequired,
-  /** Images of the SKU item */
-  images: PropTypes.arrayOf(PropTypes.shape({
-    /** URL of source Image */
-    imageUrl: PropTypes.string.isRequired,
-    /** Brief description of the image */
-    imageLabel: PropTypes.string,
-  })).isRequired,
-  /** SkuID */
-  itemId: PropTypes.string.isRequired,
-  /** List of products specifications names */
-  variations: PropTypes.arrayOf(PropTypes.string),
+const skuPropType = variations => {
+  return PropTypes.shape({
+    /** Name of the SKU Item */
+    name: PropTypes.string.isRequired,
+    /** Images of the SKU item */
+    images: PropTypes.arrayOf(
+      PropTypes.shape({
+        /** URL of source Image */
+        imageUrl: PropTypes.string.isRequired,
+        /** Brief description of the image */
+        imageLabel: PropTypes.string,
+      })
+    ).isRequired,
+    /** SkuID */
+    itemId: PropTypes.string.isRequired,
+    /** List of products specifications names */
+    variations,
+  })
+}
+
+const variationsForContainer = PropTypes.arrayOf(
+  PropTypes.shape({
+    name: PropTypes.string,
+    values: PropTypes.arrayOf(PropTypes.string),
+  })
+)
+
+const variationParsed = PropTypes.shape({
+  name: PropTypes.string,
+  options: PropTypes.arrayOf(skuPropType(PropTypes.arrayOf(PropTypes.string))),
 })
+
+export const variationComponentPropTypes = {
+  variation: variationParsed,
+  onSelectItem: PropTypes.func,
+  maxSkuPrice: PropTypes.number,
+  isSelected: PropTypes.func,
+}
 
 export const SKUSelectorContainerPropTypes = {
   /** Product's slug */
   productSlug: PropTypes.string,
   /** SKU selected */
-  skuSelected: skuPropType.isRequired,
+  skuSelected: skuPropType(variationsForContainer).isRequired,
   /** List of SKU Items */
-  skuItems: PropTypes.arrayOf(skuPropType).isRequired,
+  skuItems: PropTypes.arrayOf(skuPropType(variationsForContainer)).isRequired,
   /** Callback that is called when an SKU is selected */
   onSKUSelected: PropTypes.func,
   /** If true, show secondary options (if present), even when main variation is not picked yet. Default to true */
   shouldShowSecondary: PropTypes.bool,
 }
 
-const variationPropTypes = PropTypes.shape({
-  name: PropTypes.string.isRequired,
-  value: PropTypes.string,
-  options: PropTypes.arrayOf(skuPropType),
-})
-
 export const SKUSelectorPropTypes = {
   /** Function to go to the product page of a given sku */
   onSelectSku: PropTypes.func.isRequired,
   /** Name and list of options of the main variation */
-  mainVariation: variationPropTypes,
+  mainVariation: variationParsed,
   /** Name and list of options of the secondary variation */
-  secondaryVariation: variationPropTypes,
+  secondaryVariation: variationParsed,
   /** Max price find on the sku list */
   maxSkuPrice: PropTypes.number.isRequired,
   /** If true, show secondary options (if present), even when main variation is not picked yet */
