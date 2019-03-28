cf77cbeb65529d4208c278a53940083584604b7e
Gustavo Rosolem <guspace92@hotmail.com>
2019-03-28 14:22:49 -0300

    Allow choose the background and add translate

--- react/components/NotificationBar/index.js
+++ react/components/NotificationBar/index.js
@@ -1,15 +1,34 @@
 import React, { memo } from 'react'
 import { bool, string, oneOf } from 'prop-types'
 import classNames from 'classnames'
+import { values } from 'ramda'
 
 import styles from './styles.css'
 
-const NotificationBar = ({
-  content
-}) => {
+const getEnumValues = enumObject => values(enumObject).map(({ value }) => value)
+const getEnumNames = enumObject => values(enumObject).map(({ name }) => name)
 
+const colorPatterns = {
+  ACTION_PRIMARY: {
+    name: 'editor.notification-bar.colorPatterns.action_primary',
+    value: 'action-primary',
+  },
+  BASE_INVERTED: {
+    name: 'editor.notification-bar.colorPatterns.base_inverted',
+    value: 'base-inverted',
+  },
+}
+
+const NotificationBar = ({
+  content,
+  colorPattern
+}) => {
+  const containerClasses = classNames(`${styles.notificationBarContainer}`, {
+    [`bg-action-primary c-on-action-primary`]: colorPattern === colorPatterns.ACTION_PRIMARY.value,
+    [`bg-base--inverted c-on-base--inverted`]: colorPattern === colorPatterns.BASE_INVERTED.value,
+  })
   return content && (
-    <div className={`${styles.notificationBarContainer} bg-action-primary c-on-action-primary`}>
+    <div className={containerClasses}>
       <div className={`${styles.notificationBarContent} min-h-large flex items-center justify-center`}>
         {content}
       </div>
@@ -18,11 +37,13 @@ const NotificationBar = ({
 }
 
 NotificationBar.propTypes = {
-  content: string
+  content: string,
+  colorPattern: oneOf(getEnumValues(colorPatterns)),
 }
 
 NotificationBar.defaultProps = {
-  content: ''
+  content: '',
+  colorPattern: colorPatterns.ACTION_PRIMARY.value
 }
 
 NotificationBar.schema = {
@@ -35,6 +56,14 @@ NotificationBar.schema = {
       description: 'editor.notification-bar.content.description',
       type: 'string',
       default: '',
+    },
+    colorPattern: {
+      title: 'editor.notification-bar.colorPattern.title',
+      description: 'editor.notification-bar.colorPattern.description',
+      type: 'string',
+      default: colorPatterns.ACTION_PRIMARY.value,
+      enum: getEnumValues(colorPatterns),
+      enumNames: getEnumNames(colorPatterns),
     },
   },
 }
