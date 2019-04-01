5775649c1a98c24c43c5eed9aa4ebf1000cdf5d9
Juan Almeida <juan.satyro@gmail.com>
2019-04-01 13:42:09 -0300

    Using setQuery instead navigate in sku-selector

--- react/components/SKUSelector/index.js
+++ react/components/SKUSelector/index.js
@@ -14,14 +14,13 @@ import {
  * Display a list of SKU items of a product and its specifications.
  */
 class SKUSelectorContainer extends Component {
-
   static defaultProps = {
     alwaysShowSecondary: true,
   }
 
   state = { mainVariation: null, secondaryVariation: null }
 
-  buildVariations = (rawSkuSelected) => {
+  buildVariations = rawSkuSelected => {
     const skuSelected = rawSkuSelected && parseSku(rawSkuSelected)
     const skuItems =
       this.props.skuItems && this.props.skuItems.map(sku => parseSku(sku))
@@ -51,18 +50,24 @@ class SKUSelectorContainer extends Component {
   }
 
   componentDidMount() {
-    this.setState(this.buildVariations(this.props.skuSelected || this.props.skuItems[0]))
+    this.setState(
+      this.buildVariations(this.props.skuSelected || this.props.skuItems[0])
+    )
   }
 
   handleSkuSelection = (isMainVariation, skuId) => {
-
-    const selectedSku = this.props.skuItems.find(({ itemId }) => itemId === skuId)
+    const selectedSku = this.props.skuItems.find(
+      ({ itemId }) => itemId === skuId
+    )
     const variations = this.buildVariations(selectedSku)
 
     variations.mainVariation.value = selectedSku.variations[0].values[0]
     variations.secondaryVariation.value = null
-    // If there is secondary variation and there is only one option, assign skuId value to it  
-    if (variations.secondaryVariation.options && variations.secondaryVariation.options.length === 1) {
+    // If there is secondary variation and there is only one option, assign skuId value to it
+    if (
+      variations.secondaryVariation.options &&
+      variations.secondaryVariation.options.length === 1
+    ) {
       variations.secondaryVariation.value = skuId
     }
     if (!isMainVariation) {
@@ -72,25 +77,18 @@ class SKUSelectorContainer extends Component {
     this.setState(variations)
 
     const isSecondaryPicked = !!variations.secondaryVariation.value
-    
+
     this.props.onSKUSelected
       ? this.props.onSKUSelected(skuId, isMainVariation, isSecondaryPicked)
-      : this.redirectToSku(skuId, isMainVariation)
+      : this.redirectToSku(skuId)
   }
 
-  redirectToSku(skuId, isMainVariation) {
+  redirectToSku(skuId) {
     const {
-      runtime: { navigate },
+      runtime: { setQuery },
     } = this.props
-    const slug = this.props.productSlug
 
-    navigate({
-      page: 'store.product',
-      params: { slug },
-      query: `skuId=${skuId}`,
-      scrollOptions: false,
-      replace: !isMainVariation
-    })
+    setQuery({ skuId })
   }
 
   render() {
