f93bb1a0acaea91cbd70d9d9136d23bc5f29d2f0
Sahan Jayawardana <sahan@clouda.io>
2019-04-02 13:16:41 +0530

    [Title] Allow CSS overrides

--- react/components/Title/index.js
+++ react/components/Title/index.js
@@ -2,6 +2,7 @@ import PropTypes from 'prop-types'
 import React, { Component } from 'react'
 
 import { LEFT, RIGHT, CENTER } from './constants.js'
+import title from './title.css'
 
 /**
  * Name component. Show name and relevant SKU information of the Product Summary
@@ -22,8 +23,8 @@ class Title extends Component {
     const { content, alignment } = this.props
 
     return (
-      <div className="flex justify-between">
-        <div className="w-100 ma1">
+      <div className={`${title.titleContainer} flex justify-between`}>
+        <div className={`${title.titleBox} w-100 ma1`}>
           {alignment == LEFT && <h3 className="tl">{content}</h3>}
           {alignment == CENTER && <h3 className="tc">{content}</h3>}
           {alignment == RIGHT && <h3 className="tr">{content}</h3>}
