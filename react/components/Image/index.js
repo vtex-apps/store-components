1f380f7ccdc3ef6365cb71a75a81190bd27feb35
Gustavo Rosolem <guspace92@hotmail.com>
2019-04-10 16:58:59 -0300

    Add blockClass

--- react/components/Image/index.js
+++ react/components/Image/index.js
@@ -1,19 +1,18 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import { generateBlockClass } from '@vtex/css-handles'
 
 import styles from './styles.css'
 
-class Image extends Component {
-  render() {
-    const { src, alt, maxWidth, maxHeight, srcset, sizes } = this.props
-    const maxDimensions = {
-      maxWidth: maxWidth,
-      maxHeight: maxHeight,
-    }
-    return (
-      <img src={src} srcset={srcset} sizes={sizes} alt={alt} style={maxDimensions} className={styles.imageElement} />
-    )
+const Image = ({ src, alt, maxWidth, maxHeight, srcset, sizes, blockClass }) => {
+  const classes = generateBlockClass(styles.imageElement, blockClass)
+  const maxDimensions = {
+    maxWidth: maxWidth,
+    maxHeight: maxHeight,
   }
+  return (
+    <img src={src} srcset={srcset} sizes={sizes} alt={alt} style={maxDimensions} className={classes} />
+  )
 }
 
 Image.propTypes = {
@@ -23,10 +22,10 @@ Image.propTypes = {
   maxHeight: PropTypes.string,
   srcset: PropTypes.string,
   sizes: PropTypes.string,
+  blockClass: PropTypes.string,
 }
 
 Image.defaultProps = {
-  src: '',
   alt: '',
   maxWidth: 'none',
   maxHeight: 'none',
@@ -49,6 +48,12 @@ Image.schema = {
     alt: {
       type: 'string',
       title: 'editor.image.alt.title',
+    },
+    blockClass: {
+      title: 'editor.blockClass.title',
+      description: 'editor.blockClass.description',
+      type: 'string',
+      isLayout: true,
     },
   },
 }
