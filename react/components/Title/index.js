32c86a6825047f09e0704ddf241a13614ffeb667
Sahan Jayawardana <sahan@clouda.io>
2019-04-12 09:23:25 +0530

    [Title] Use a function component

--- react/components/Title/index.js
+++ react/components/Title/index.js
@@ -1,37 +1,30 @@
 import PropTypes from 'prop-types'
-import React, { Component } from 'react'
+import React from 'react'
 
 import { LEFT, RIGHT, CENTER } from './constants.js'
 import title from './title.css'
 
-/**
- * Title component - Show text in an HTML heading
- */
-class Title extends Component {
-  static propTypes = {
-    /** Text to be displayed */
-    content: PropTypes.string,
-    /** Alignment of the title */
-    alignment: PropTypes.oneOf([LEFT, RIGHT, CENTER]).isRequired,
-  }
-
-  static defaultProps = {
-    alignment: CENTER,
-  }
-
-  render() {
-    const { content, alignment } = this.props
-
-    return (
-      <div className={`${title.titleContainer} flex justify-between`}>
-        <div className={`${title.titleBox} w-100 ma1 c-muted-1`}>
-          {alignment == LEFT && <h3 className="tl t-heading-3">{content}</h3>}
-          {alignment == CENTER && <h3 className="tc t-heading-3">{content}</h3>}
-          {alignment == RIGHT && <h3 className="tr t-heading-3">{content}</h3>}
-        </div>
+const Title = ({ content, alignment }) => {
+  return (
+    <div className={`${title.titleContainer} flex justify-between`}>
+      <div className={`${title.titleBox} w-100 ma1 c-muted-1`}>
+        {alignment == LEFT && <h3 className="tl t-heading-3">{content}</h3>}
+        {alignment == CENTER && <h3 className="tc t-heading-3">{content}</h3>}
+        {alignment == RIGHT && <h3 className="tr t-heading-3">{content}</h3>}
       </div>
-    )
-  }
+    </div>
+  )
+}
+
+Title.propTypes = {
+  /** Text to be displayed */
+  content: PropTypes.string,
+  /** Alignment of the title */
+  alignment: PropTypes.oneOf([LEFT, RIGHT, CENTER]).isRequired,
+}
+
+Title.defaultProps = {
+  alignment: CENTER,
 }
 
 Title.schema = {
