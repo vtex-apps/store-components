ec4f8132c06461b09cb4b9d6545ef71bbe5f9853
Joao Fidelis <joaogfidelis@gmail.com>
2019-04-18 15:49:25 -0300

    make UserAddress use modal block

--- react/components/UserAddress/index.js
+++ react/components/UserAddress/index.js
@@ -1,135 +1,125 @@
 import PropTypes from 'prop-types'
 import React from 'react'
-import { FormattedMessage } from 'react-intl'
+import { injectIntl, FormattedMessage } from 'react-intl'
 import { graphql } from 'react-apollo'
 import gql from 'graphql-tag'
 import { compose, pathOr } from 'ramda'
+import { ExtensionPoint, useChildBlock } from 'vtex.render-runtime'
 
 import { IconLocationMarker } from 'vtex.store-icons'
 import Container from '../Container'
 
-class UserAddress extends React.Component {
-  static propTypes = {
-    variation: PropTypes.oneOf(['inline', 'bar']).isRequired,
-    onChange: PropTypes.func,
-    orderForm: PropTypes.object.isRequired,
-  }
+const AddressInfo = ({ inverted, inline, orderForm, pickupPointQuery, intl }) => {
+  const { shippingData } = orderForm
+  if (!shippingData || !shippingData.address) return
 
-  renderAddressInfo = ({ inverted, inline }) => {
-    const {
-      orderForm: { shippingData },
-      onChange,
-    } = this.props
+  const { street, number, complement, addressType } = shippingData.address
 
-    if (!shippingData || !shippingData.address) return
+  const displayStreet = number ? `${street}, ${number}` : street
 
-    const { street, number, complement, addressType } = shippingData.address
+  const displayAddress =
+    complement && complement !== ''
+      ? `${displayStreet} - ${complement}`
+      : `${displayStreet}`
 
-    const displayStreet = number ? `${street}, ${number}` : street
+  const isPickup = addressType === 'pickup'
+  const friendlyName = pathOr(
+    '',
+    ['pickupPoint', 'friendlyName'],
+    pickupPointQuery
+  )
 
-    const displayAddress =
-      complement && complement !== ''
-        ? `${displayStreet} - ${complement}`
-        : `${displayStreet}`
+  const hasModal = !!useChildBlock({ id: 'modal' })
 
-    const isPickup = addressType === 'pickup'
-    const friendlyName = pathOr(
-      '',
-      ['pickupPointQuery', 'pickupPoint', 'friendlyName'],
-      this.props
-    )
-
-    return (
-      <div
-        className={`flex ${inline ? 'items-end' : 'items-center flex-auto'}`}
-      >
-        <div className="flex flex-auto">
+  return (
+    <div
+      className={`flex ${inline ? 'items-end' : 'items-center flex-auto'}`}
+    >
+      <div className="flex flex-auto">
+        <div
+          className={`mr3 flex items-center ${
+            inverted ? 'c-on-base--inverted' : 'c-muted-2'
+          }`}
+        >
+          <IconLocationMarker size={27} viewBox={'0 0 21 27'} />
+        </div>
+        <div className="flex flex-auto flex-column">
           <div
-            className={`mr3 flex items-center ${
+            className={`t-small ${
               inverted ? 'c-on-base--inverted' : 'c-muted-2'
             }`}
           >
-            <IconLocationMarker size={27} viewBox={'0 0 21 27'} />
+            {isPickup ? (
+              <FormattedMessage
+                id="user-address.pickup"
+                values={{ name: friendlyName }}
+              />
+            ) : (
+              <FormattedMessage id="user-address.order" />
+            )}
           </div>
-          <div className="flex flex-auto flex-column">
-            <div
-              className={`t-small ${
-                inverted ? 'c-on-base--inverted' : 'c-muted-2'
-              }`}
-            >
-              {isPickup ? (
-                <FormattedMessage
-                  id="user-address.pickup"
-                  values={{ name: friendlyName }}
-                />
-              ) : (
-                <FormattedMessage id="user-address.order" />
-              )}
-            </div>
-            <div className="truncate">{displayAddress}</div>
-          </div>
+          <div className="truncate">{displayAddress}</div>
         </div>
-        {onChange && (
-          <React.Fragment>
-            <div
-              className={`bl bw1 mh4 ${inline ? 'nb2' : ''} ${
-                inverted ? 'b--on-base--inverted' : 'b--muted-5'
-              }`}
-              style={{
-                height: '1.5rem',
-              }}
+      </div>
+      {hasModal && (
+        <React.Fragment>
+          <div
+            className={`bl bw1 mh4 ${inline ? 'nb2' : ''} ${
+              inverted ? 'b--on-base--inverted' : 'b--muted-5'
+            }`}
+            style={{
+              height: '1.5rem',
+            }}
+          />
+          <div className="flex items-center">
+            <ExtensionPoint 
+              id="modal"
+              centered
+              buttonLabel={intl.formatMessage({ id: 'user-address.change' })}
+              buttonClass={inverted ? 'c-on-base--inverted' : 'c-action-primary'}
             />
-            <div className="flex items-center">
-              <div
-                className={`t-action pointer pv3 nv3 ph4 nh4 ${
-                  inverted ? 'c-on-base--inverted' : 'c-action-primary'
-                }`}
-                role="button"
-                onClick={onChange}
-              >
-                <FormattedMessage id="user-address.change" />
-              </div>
-            </div>
-          </React.Fragment>
-        )}
-      </div>
-    )
+          </div>
+        </React.Fragment>
+      )}
+    </div>
+  )
+}
+
+const UserAddress = ({ variation, orderForm, pickupPointQuery, intl }) => {
+  const { shippingData } = orderForm
+
+  if (!shippingData || !shippingData.address) {
+    return null
   }
 
-  render() {
-    const { variation, orderForm } = this.props
+  const isInline = variation === 'inline'
+  const isInverted = !isInline
 
-    const { shippingData } = orderForm
+  const content = <AddressInfo intl={intl} inverted={isInverted} inline={isInline} orderForm={orderForm} pickupPointQuery={pickupPointQuery} />
 
-    if (!shippingData || !shippingData.address) {
-      return null
-    }
+  return isInline ? (
+    <div
+      className="ph5"
+      style={{
+        maxWidth: '30rem',
+      }}
+    >
+      {content}
+    </div>
+  ) : (
+    <div className="bg-base--inverted c-on-base--inverted flex ph5 pointer pv3">
+      <Container className="flex justify-center w-100 left-0">
+        <div className="w-100 mw9 flex">
+          {content}
+        </div>
+      </Container>
+    </div>
+  )
+}
 
-    const isInline = variation === 'inline'
-    const isInverted = !isInline
-
-    const content = this.renderAddressInfo({
-      inverted: isInverted,
-      inline: isInline,
-    })
-
-    return isInline ? (
-      <div
-        className="ph5"
-        style={{
-          maxWidth: '30rem',
-        }}
-      >
-        {content}
-      </div>
-    ) : (
-      <div className="bg-base--inverted c-on-base--inverted flex ph5 pointer pv3">
-        <Container className="flex justify-center w-100 left-0">
-          <div className="w-100 mw9 flex">{content}</div>
-        </Container>
-      </div>
-    )
-  }
+UserAddress.propTypes = {
+  variation: PropTypes.oneOf(['inline', 'bar']).isRequired,
+  orderForm: PropTypes.object.isRequired,
 }
 
 const withShippingDataQuery = graphql(
@@ -169,6 +159,7 @@ const withPickupPointQuery = graphql(
 )
 
 export default compose(
+  injectIntl,
   withShippingDataQuery,
   withPickupPointQuery
 )(UserAddress)
