5f6f20eca09cfc7daa9cde0d8fd8462ad102e7c0
Joao Fidelis <joaogfidelis@gmail.com>
2019-05-08 17:40:03 -0300

    adapt Logo component to have storefront schema

--- react/components/Logo/index.js
+++ react/components/Logo/index.js
@@ -16,21 +16,24 @@ const wrapWithLink = (href, element) =>
 /**
  * Logo of the store
  */
-const Logo = ({ href, isMobile, url, width, height, title }) => {
-  const { account } = useRuntime()
+const Logo = ({ href, url, width, height, title, mobileWidth, mobileHeight }) => {
+  const { account, hints: { mobile } } = useRuntime()
 
   const logoClassNames = classNames('store-logo', styles.logoContainer, {
-    [styles.sizeDesktop]: !isMobile,
-    [styles.sizeMobile]: isMobile,
+    [styles.sizeDesktop]: !mobile,
+    [styles.sizeMobile]: mobile,
   })
+
+  const imgWidth = mobile && mobileWidth ? mobileWidth : width
+  const imgHeight = mobile && mobileHeight ? mobileHeight : height
 
   const logo = (
     <span className={`${logoClassNames} pv4 ph6`}>
       {url ? (
         <img
           src={url.replace(/{{account}}/g, account)}
-          width={width}
-          height={height}
+          width={imgWidth}
+          height={imgHeight}
           alt={title}
         />
       ) : (
@@ -51,6 +54,53 @@ Logo.propTypes = {
   width: PropTypes.number,
   /** Logo's height */
   height: PropTypes.number,
+}
+
+Logo.schema = {
+  title: 'admin/editor.logo.title',
+  description: 'admin/editor.logo.description',
+  type: 'object',
+  properties: {
+    href: {
+      title: 'admin/editor.logo.href.title',
+      description: 'admin/editor.logo.href.description',
+      type: 'string',
+    },
+    url: {
+      title: 'admin/editor.logo.url.title',
+      description: 'admin/editor.logo.url.description',
+      type: 'string',
+    },
+    width: {
+      title: 'admin/editor.logo.width.title',
+      description: 'admin/editor.logo.width.description',
+      type: 'number',
+      isLayout: true,
+    },
+    height: {
+      title: 'admin/editor.logo.height.title',
+      description: 'admin/editor.logo.height.description',
+      type: 'number',
+      isLayout: true,
+    },
+    title: {
+      title: 'admin/editor.logo.title.title',
+      description: 'admin/editor.logo.title.description',
+      type: 'string',
+    },
+    mobileWidth: {
+      title: 'admin/editor.logo.mobileWidth.title',
+      description: 'admin/editor.logo.mobileWidth.description',
+      type: 'number',
+      isLayout: true,
+    },
+    mobileHeight: {
+      title: 'admin/editor.logo.mobileHeight.title',
+      description: 'admin/editor.logo.mobileHeight.description',
+      type: 'number',
+      isLayout: true,
+    },
+  }
 }
 
 export default Logo
