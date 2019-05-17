b2fe69ae102185492dea5769563e9fd68237a73c
Thayanne Sousa <thayannevls@gmail.com>
2019-05-17 11:03:02 -0300

    add mocks for address-form

---
+++ react/__mocks__/vtex.address-form/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { addValidation, removeValidation } from './helpers'
+import { StyleguideInput } from './inputs'
+
+const AddressRules = props => <div className="address-rules-mock" />
+
+const AddressContainer = props => <div className="address-container-mock" />
+
+const PostalCodeGetter = props => <div className="postal-code-getter-mock" />
+
+export {AddressRules, AddressContainer, PostalCodeGetter, addValidation, removeValidation, StyleguideInput}
