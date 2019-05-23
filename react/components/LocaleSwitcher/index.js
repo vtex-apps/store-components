3faf90038a2c132d31ab2e71bbf71ac7925067fc
Thalyta Fabrine <thalyta.trindade@ccc.ufcg.edu.br>
2019-05-23 17:04:01 -0300

    Fix issues

--- react/components/LocaleSwitcher/index.js
+++ react/components/LocaleSwitcher/index.js
@@ -1,29 +1,41 @@
 import React, { useState } from 'react'
 import { useRuntime } from 'vtex.render-runtime'
-import GlobeIcon from './GlobeIcon'
+import { IconGlobe } from 'vtex.store-icons'
+
+const supportedLangs = [
+  {
+    text: 'EN',
+    id: 'en-US',
+  },
+  {
+    text: 'PT',
+    id: 'pt-BR',
+  },
+  {
+    text: 'ES',
+    id: 'es-AR',
+  },
+]
+
+function splitLocale(locale) {
+  return locale.split('-')[0]
+}
+
+function findLocale(locale) {
+  const localeObj = supportedLangs.find(
+    ({ id }) => splitLocale(id) === splitLocale(locale)
+  )
+  return localeObj || supportedLangs[0]
+}
 
 const LocaleSwitcher = () => {
-  const supportedLangs = [
-    {
-      text: 'EN',
-      id: 'en-US',
-    },
-    {
-      text: 'PT',
-      id: 'pt-BR',
-    },
-    {
-      text: 'ES',
-      id: 'es-AR',
-    },
-  ]
   const { culture, emitter } = useRuntime()
   const [openLocaleSelector, setOpenLocaleSelector] = useState(false)
   const [selectedLocale, setSelectedLocale] = useState(
     findLocale(culture.locale)
   )
 
-  const handleLocaleClick = ({ target: { id } }) => {
+  const handleLocaleClick = id => {
     emitter.emit('localesChanged', id)
     setOpenLocaleSelector(false)
     setSelectedLocale(findLocale(id))
@@ -33,17 +45,6 @@ const LocaleSwitcher = () => {
     e.preventDefault()
   }
 
-  function splitLocale(locale) {
-    return locale.split('-')[0]
-  }
-  
-  function findLocale(locale) {
-    const localeObj = supportedLangs.find(
-      ({ id }) => splitLocale(id) === splitLocale(locale)
-    )
-    return localeObj || supportedLangs[0]
-  }
-
   return (
     <div className="w3 flex items-center justify-end ml2 mr3 relative">
       <button
@@ -51,19 +52,19 @@ const LocaleSwitcher = () => {
         onBlur={() => setOpenLocaleSelector(false)}
         className={`link pa0 bg-transparent bn flex items-center pointer mr3 ${LocaleSwitcher.defaultProps.color} hover-${LocaleSwitcher.defaultProps.hoverColor}`}
       >
-        <GlobeIcon />
-        <span className={'pl2 current-color t-action--small order-1'}>{selectedLocale.text}</span>
+        <IconGlobe />
+        <span className="pl2 t-action--small order-1">{selectedLocale.text}</span>
       </button>
       <ul
         hidden={!openLocaleSelector}
-        className="absolute z-5 list top-1 w3 ph0 mh0 mv4 bg-white"
+        className="absolute z-5 list top-1 w3 ph0 mh0 mv4 bg-base"
       >
         {supportedLangs
         .filter(({ id }) => id !== selectedLocale.id)
         .map(({ id, text }) => (
           <li
-            className="t-action--small pointer f5 pa3 hover-bg-light-silver tc"
-            onClick={handleLocaleClick}
+            className="t-action--small pointer f5 pa3 hover-bg-muted-5 tc"
+            onClick={() => handleLocaleClick(id)}
             onMouseDown={handleMouseDown}
             id={id}
             key={id}
@@ -77,13 +78,11 @@ const LocaleSwitcher = () => {
 }
 
 LocaleSwitcher.propTypes = {
-  bottom: PropTypes.bool,
   color: PropTypes.string,
   hoverColor: PropTypes.string,
 }
 
 LocaleSwitcher.defaultProps = {
-  bottom: false,
   color: 'near-black',
   hoverColor: 'rebel-pink',
 }
