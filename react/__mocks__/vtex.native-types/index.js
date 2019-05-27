c5ceedcf75ff1bbfba6aebd8f67d981b53bdc2e4
Lucas Cordeiro <ecdb.lucas@gmail.com>
2019-05-27 17:31:33 -0300

    Fix tests

--- react/__mocks__/vtex.native-types/index.js
+++ react/__mocks__/vtex.native-types/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { FormattedMessage } from 'react-intl'
 
+export const formatIOMessage = ({ id }) => id
+
 export const IOMessage = props =>
   props.id ? <FormattedMessage {...props} /> : null
