829172e0aef2ffa95fe6f5dc827755064daec541
Julio Moreira <hellojuliomoreira@gmail.com>
2019-08-14 01:14:11 -0300

    Add vtex.styleguide alias, this can be improved.

---
+++ react/.storybook/__aliases__/vtex.styleguide.js
@@ -0,0 +1,109 @@
+import ActionMenu from '@vtex/styleguide/lib/ActionMenu'
+import Alert from '@vtex/styleguide/lib/Alert'
+import Box from '@vtex/styleguide/lib/Box'
+import Button from '@vtex/styleguide/lib/Button'
+import ButtonGroup from '@vtex/styleguide/lib/ButtonGroup'
+import ButtonWithIcon from '@vtex/styleguide/lib/ButtonWithIcon'
+import Card from '@vtex/styleguide/lib/Card'
+import Checkbox from '@vtex/styleguide/lib/Checkbox'
+import CheckboxGroup from '@vtex/styleguide/lib/CheckboxGroup'
+import Collapsible from '@vtex/styleguide/lib/Collapsible'
+// import ColorPicker from '@vtex/styleguide/lib/ColorPicker'
+import DatePicker from '@vtex/styleguide/lib/DatePicker'
+import Dropdown from '@vtex/styleguide/lib/Dropdown'
+import EmptyState from '@vtex/styleguide/lib/EmptyState'
+import EXPERIMENTAL_Conditions from '@vtex/styleguide/lib/EXPERIMENTAL_Conditions'
+import EXPERIMENTAL_Select from '@vtex/styleguide/lib/EXPERIMENTAL_Select'
+import FilterBar from '@vtex/styleguide/lib/FilterBar'
+import FilterOptions from '@vtex/styleguide/lib/FilterOptions'
+import FloatingActionBar from '@vtex/styleguide/lib/FloatingActionBar'
+import Input from '@vtex/styleguide/lib/Input'
+import InputCurrency from '@vtex/styleguide/lib/InputCurrency'
+import InputPassword from '@vtex/styleguide/lib/InputPassword'
+import InputSearch from '@vtex/styleguide/lib/InputSearch'
+import Layout from '@vtex/styleguide/lib/Layout'
+import Link from '@vtex/styleguide/lib/Link'
+import Menu from '@vtex/styleguide/lib/Menu'
+import Modal from '@vtex/styleguide/lib/Modal'
+import ModalDialog from '@vtex/styleguide/lib/ModalDialog'
+import MultiSelect from '@vtex/styleguide/lib/MultiSelect'
+import NumericStepper from '@vtex/styleguide/lib/NumericStepper'
+import PageBlock from '@vtex/styleguide/lib/PageBlock'
+import PageHeader from '@vtex/styleguide/lib/PageHeader'
+import Pagination from '@vtex/styleguide/lib/Pagination'
+import PasswordInput from '@vtex/styleguide/lib/PasswordInput'
+import ProgressBar from '@vtex/styleguide/lib/ProgressBar'
+import Radio from '@vtex/styleguide/lib/Radio'
+import RadioGroup from '@vtex/styleguide/lib/RadioGroup'
+import Slider from '@vtex/styleguide/lib/Slider'
+import Spinner from '@vtex/styleguide/lib/Spinner'
+import Table from '@vtex/styleguide/lib/Table'
+import Tabs from '@vtex/styleguide/lib/Tabs'
+import Tag from '@vtex/styleguide/lib/Tag'
+import Textarea from '@vtex/styleguide/lib/Textarea'
+import TimePicker from '@vtex/styleguide/lib/TimePicker'
+import {
+  ToastContext,
+  ToastProvider,
+  ToastConsumer,
+  withToast,
+} from '@vtex/styleguide/lib/ToastProvider'
+import Toggle from '@vtex/styleguide/lib/Toggle'
+import Totalizer from '@vtex/styleguide/lib/Totalizer'
+import UncoupledStatement from '@vtex/styleguide/lib/UncoupledStatement'
+import { childrenOf } from '@vtex/styleguide/lib/utils'
+
+export {
+  childrenOf,
+  ToastContext,
+  ToastProvider,
+  ToastConsumer,
+  withToast,
+  ActionMenu,
+  Alert,
+  Box,
+  Button,
+  ButtonGroup,
+  ButtonWithIcon,
+  Card,
+  Checkbox,
+  CheckboxGroup,
+  Collapsible,
+  // ColorPicker,
+  DatePicker,
+  Dropdown,
+  EmptyState,
+  EXPERIMENTAL_Conditions,
+  EXPERIMENTAL_Select,
+  FilterBar,
+  FilterOptions,
+  FloatingActionBar,
+  Input,
+  InputCurrency,
+  InputPassword,
+  InputSearch,
+  Layout,
+  Link,
+  Menu,
+  Modal,
+  ModalDialog,
+  MultiSelect,
+  NumericStepper,
+  PageBlock,
+  PageHeader,
+  Pagination,
+  PasswordInput,
+  ProgressBar,
+  Radio,
+  RadioGroup,
+  Slider,
+  Spinner,
+  Table,
+  Tabs,
+  Tag,
+  Textarea,
+  TimePicker,
+  Toggle,
+  Totalizer,
+  UncoupledStatement,
+}
