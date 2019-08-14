6096eff70feb0524f3905f5cf2845b744eee65a0
Julio Moreira <hellojuliomoreira@gmail.com>
2019-08-14 01:13:36 -0300

    Add Storybook base configuration.

---
+++ react/.storybook/webpack.config.js
@@ -0,0 +1,32 @@
+const webpack = require('webpack')
+const path = require('path')
+
+module.exports = async ({ config }) => {
+  config.resolve.modules = [
+    ...config.resolve.modules,
+    path.resolve(__dirname, '__aliases__'),
+    '__mocks__',
+  ]
+  config.module.rules = [
+    ...config.module.rules,
+    {
+      test: /\.(graphql|gql)$/,
+      exclude: /node_modules/,
+      loader: 'graphql-tag/loader',
+    },
+    {
+      test: /\.(ts|tsx)$/,
+      use: [
+        {
+          loader: require.resolve('awesome-typescript-loader'),
+        },
+        // Optional
+        {
+          loader: require.resolve('react-docgen-typescript-loader'),
+        },
+      ],
+    },
+  ]
+  config.resolve.extensions = [...config.resolve.extensions, '.ts', '.tsx']
+  return config
+}
