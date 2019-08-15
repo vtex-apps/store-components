23c2dc371e07fb73fb746c65b31f629b0e46c7f9
Julio Moreira <hellojuliomoreira@gmail.com>
2019-08-15 00:43:00 -0300

    Add i18n support.

--- react/.storybook/config.js
+++ react/.storybook/config.js
@@ -1,20 +1,43 @@
 import React from 'react'
-import { configure, addDecorator } from '@storybook/react'
-
-const { IntlProvider } = require('react-intl')
-const { MockedProvider } = require('react-apollo/test-utils')
+import { MockedProvider } from 'react-apollo/test-utils'
+import { withI18n } from 'storybook-addon-i18n'
+import { configure, addDecorator, addParameters } from '@storybook/react'
+import { LocaleProvider } from './LocaleProvider'
 
 import 'vtex-tachyons'
+
+import messages_en from '../../messages/en.json'
+import messages_pt from '../../messages/pt.json'
+import messages_es from '../../messages/es.json'
+const messages = {
+  en: messages_en,
+  pt: messages_pt,
+  es: messages_es,
+}
+
+addDecorator(withI18n)
+addDecorator(storyFn => (
+  <MockedProvider resolvers={{}}>{storyFn()}</MockedProvider>
+))
+
+const language = navigator.language.split(/[-_]/)[0] // language without region code
+
+addParameters({
+  i18n: {
+    provider: LocaleProvider,
+    providerProps: {
+      defaultLocale: 'en',
+      messages: messages,
+    },
+    supportedLocales: ['en', 'pt', 'es'],
+    providerLocaleKey: 'locale',
+  },
+})
 
 // automatically import all files ending in *.stories.js
 const req = require.context('../stories', true, /\.stories\.js$/)
 function loadStories() {
   req.keys().forEach(filename => req(filename))
 }
-
-addDecorator(storyFn => (
-  <MockedProvider resolvers={{}}>{storyFn()}</MockedProvider>
-))
-addDecorator(storyFn => <IntlProvider locale="en">{storyFn()}</IntlProvider>)
 
 configure(loadStories, module)
