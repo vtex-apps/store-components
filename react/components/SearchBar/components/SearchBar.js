fc7c2dd448728b63d0affcec7d3101e7951ff841
Hiago Lucas Cardeal de Melo Silva <hiago@biggy.com.br>
2019-10-02 13:05:22 -0300

    Change ResultsList to allow customSearchPageUrl

--- react/components/SearchBar/components/SearchBar.js
+++ react/components/SearchBar/components/SearchBar.js
@@ -169,6 +169,7 @@ const SearchBar = ({
                     highlightedIndex,
                     closeMenu,
                     onClearInput,
+                    customSearchPageUrl,
                   }}
                 />
               </Overlay>
