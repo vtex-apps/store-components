ccabcb16fa9621e61f972de73643aecf7fbab138
victorhmp <victor2142@gmail.com>
2019-10-03 18:04:41 -0300

    Update CSS handles on ProductDescription

--- react/components/ProductDescription/index.js
+++ react/components/ProductDescription/index.js
@@ -2,10 +2,15 @@ import React, { memo, useMemo } from 'react'
 import PropTypes from 'prop-types'
 import { FormattedMessage } from 'react-intl'
 import HtmlParser from 'react-html-parser'
+import { useCssHandles } from 'vtex.css-handles'
 
 import GradientCollapse from '../GradientCollapse/index'
 
-import styles from './styles.css'
+const CSS_HANDLES = [
+  'productDescriptionContainer',
+  'productDescriptionTitle',
+  'productDescriptionText',
+]
 
 /**
  * Product Description Component.
@@ -20,13 +25,21 @@ const ProductDescription = ({ description, collapseContent }) => {
     description,
   ])
 
+  const handles = useCssHandles(CSS_HANDLES)
+
   return (
-    <div className={styles.productDescriptionContainer}>
+    <div className={handles.productDescriptionContainer}>
       <FormattedMessage id="store/product-description.title">
-        {txt => <h2 className="t-heading-5 mb5 mt0">{txt}</h2>}
+        {txt => (
+          <h2
+            className={`${handles.productDescriptionTitle} t-heading-5 mb5 mt0`}
+          >
+            {txt}
+          </h2>
+        )}
       </FormattedMessage>
 
-      <div className="c-muted-1">
+      <div className={`${handles.productDescriptionText} c-muted-1`}>
         {collapseContent ? (
           <GradientCollapse collapseHeight={220}>
             {descriptionParsed}
