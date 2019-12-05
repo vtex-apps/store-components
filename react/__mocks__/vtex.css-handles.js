efba4ebddc9a1d298b1fd9b9ef0a6fd09692011d
Rafael Klynger <rafael.dantas@vtex.com.br>
2019-12-05 15:38:45 -0300

    Update mock

--- react/__mocks__/vtex.css-handles.js
+++ react/__mocks__/vtex.css-handles.js
@@ -13,11 +13,15 @@ export function applyModifiers(baseClass, modifier) {
   return `${baseClass}--${modifier}`
 }
 
-export const withCssHandles = () => Comp => {
-  return class extends React.Component {
-    static displayName = 'withCssHandles'
-    render() {
-      return <Comp cssHandles={{}} {...this.props} />
-    }
+export const withCssHandles = (handles = [], options) => Component => {
+  const EnhancedComponent = props => {
+    const cssHandles = useCssHandles(handles, options)
+
+    return <Component cssHandles={cssHandles} {...props} />
   }
+  
+  const displayName = Component.displayName || Component.name || 'Component'
+  EnhancedComponent.displayName = `withCssHandles(${displayName})`
+
+  return EnhancedComponent
 }
