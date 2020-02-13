8d871aa3f8b7212384fa4daf8c207fac507f5b48
Christian Kaisermann <christian@kaisermann.me>
2020-02-13 11:12:17 -0300

    Use enum instead of hard-coded value

--- react/components/SKUSelector/Wrapper.tsx
+++ react/components/SKUSelector/Wrapper.tsx
@@ -135,7 +135,8 @@ const SKUSelectorWrapper: StorefrontFC<Props> = props => {
     pick(['imageHeight', 'imageWidth'], props)
   )
 
-  const shouldSelectInitialSKU = props.initialSelection !== 'empty'
+  const shouldSelectInitialSKU =
+    props.initialSelection !== InitialSelectionType.empty
 
   const skuItems =
     props.skuItems != null
