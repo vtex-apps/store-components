db7e009230a1d60ebd5f9f2dc4e7a61f3ae0b029
Sebastian Sanchez Vega <sebastiansanchezvega@gmail.com>
2020-04-14 19:19:39 -0300

    Component done

--- react/components/BackToTopButton/index.tsx
+++ react/components/BackToTopButton/index.tsx
@@ -1,34 +1,74 @@
-import React, {  useCallback } from 'react'
+import React, { useCallback, useState, useEffect } from 'react'
 import { Button } from 'vtex.styleguide'
 import { FormattedMessage } from 'react-intl'
 import styles from './styles.css'
+import { Icon } from 'vtex.store-icons'
 
 interface Props {
-  topPixel?: number,
-  rightPosition?: string,
+  showAfterPixelY?: number
+  rightPosition?: string
   bottomPosition?: string
+  isIconStyle?: boolean
 }
 
-const BackToTopButton: StorefrontFC<Props> = (props) => {
+const BackToTopButton: StorefrontFC<Props> = props => {
   const handleBackTop = useCallback(() => {
     window.scroll({ top: 0, left: 0, behavior: 'smooth' })
   }, [])
+  const [isShowed, setIsShowed] = useState(false)
+  const [scrollY, setScrollY] = useState(0)
+  useEffect(() => {
+    function watchScroll() {
+      window.addEventListener('scroll', scrollValue)
+    }
+    watchScroll()
+    return () => {
+      window.removeEventListener('scroll', scrollValue)
+    }
+  })
 
-  return (
+  useEffect(() => {
+    if (scrollY > props.showAfterPixelY!) {
+      setIsShowed(true)
+    } else {
+      setIsShowed(false)
+    }
+  })
+
+  function scrollValue() {
+    setScrollY(window.pageYOffset)
+  }
+
+  return !props.isIconStyle ? (
     <div
-      className={`${styles.BackToTopButtonContainer} z-999 fixed`}
+      className={`${styles.BackToTopButtonContainer} ${
+        isShowed ? styles.BackToTopButtonActive : styles.BackToTopButtonHidden
+      } z-999 fixed`}
       style={{ right: props.rightPosition, bottom: props.bottomPosition }}
     >
       <Button onClick={handleBackTop} size="regular">
         <FormattedMessage id="store/editor.action-go-to" />
       </Button>
     </div>
+  ) : (
+    <div
+      className={`${styles.BackToTopButtonContainer} ${
+        isShowed ? styles.BackToTopButtonActive : styles.BackToTopButtonHidden
+      } z-999 fixed`}
+      style={{ right: props.rightPosition, bottom: props.bottomPosition }}
+    >
+      <Button onClick={handleBackTop} size="regular">
+        <Icon id="nav-caret--up" size="16" />
+      </Button>
+    </div>
   )
 }
 
 BackToTopButton.defaultProps = {
-  rightPosition: "0",
-  bottomPosition: "2rem"
+  rightPosition: '2rem',
+  bottomPosition: '2rem',
+  showAfterPixelY: 600,
+  isIconStyle: false,
 }
 
 export default BackToTopButton
