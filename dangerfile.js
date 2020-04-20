104cae15456babefae5ac8c8996611fd43ae400a
Christian Kaisermann <christian@kaisermann.me>
2020-04-20 16:16:26 -0300

    Add danger and @vtex/danger and add dangerfile.js to .eslintignore

--- dangerfile.js
+++ dangerfile.js
@@ -1,4 +1,9 @@
 // docs at https://github.com/vtex/danger
-const { assert } = require('@vtex/danger');
+const { verify } = require('@vtex/danger')
 
-assert();
+verify({
+  keepachangelog: {
+    // set to 'true' to require a new version defined in the changelog change
+    changeVersion: true,
+  },
+})
