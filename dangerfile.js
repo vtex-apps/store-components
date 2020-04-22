435e5637f8181d7f2463c8536a7e1b091bad71df
Christian Kaisermann <christian@kaisermann.me>
2020-04-22 12:24:26 -0300

    Revert "Add pull request actions"

--- dangerfile.js
+++ dangerfile.js
@@ -1,9 +1,4 @@
 // docs at https://github.com/vtex/danger
-const { verify } = require('@vtex/danger')
+const { assert } = require('@vtex/danger');
 
-verify({
-  keepachangelog: {
-    // set to 'true' to require a new version defined in the changelog change
-    changeVersion: true,
-  },
-})
+assert();
