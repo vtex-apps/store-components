99bd2cfcc97313ad0625e0ed1cd0745ae1911c76
klynger <rafael.klynger@gmail.com>
2020-06-08 16:05:35 -0300

    Fix eslint problems of the 'Slider' component

--- react/components/Slider/components/Arrow.js
+++ react/components/Slider/components/Arrow.js
@@ -16,6 +16,7 @@ export default class Arrow extends Component {
     })
 
     return (
+      // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions
       <div
         className={`${arrowClasses}`}
         style={{ ...style }}
--- react/components/Slider/index.js
+++ react/components/Slider/index.js
@@ -7,7 +7,6 @@ import { NoSSR } from 'vtex.render-runtime'
 import Dots from './components/Dots'
 import Arrow from './components/Arrow'
 import getItemsPerPage from './utils/ItemsPerPage'
-
 import './global.css'
 import slider from './slider.css'
 
@@ -111,6 +110,7 @@ export default class SlickSlider extends Component {
       </ReactResizeDetector>
     )
     if (this.props.ssrFallback) {
+      // eslint-disable-next-line react/jsx-handler-names
       return <NoSSR onSSR={this.props.ssrFallback}>{component}</NoSSR>
     }
     return component
