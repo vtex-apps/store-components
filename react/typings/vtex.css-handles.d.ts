5265454ab90bd6b877444b82f7060a866412032a
klynger <rafael.klynger@gmail.com>
2020-06-16 15:41:44 -0300

    Remove unnecessary module declaration in typings folder

--- react/typings/vtex.css-handles.d.ts
+++
@@ -1,5 +0,0 @@
-declare module 'vtex.css-handles' {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const useCssHandles: any
-  const applyModifiers: (handle: string, modifier: string | string[]) => string
-}
