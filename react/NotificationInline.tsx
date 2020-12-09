eab3f1bf0d174ea64de6d530436cbebe1a731432
Ícaro <icaro.azevedo@vtex.com.br>
2020-12-09 16:52:25 -0300

    Refactoring Notification to new css handles

--- react/NotificationInline.tsx
+++ react/NotificationInline.tsx
@@ -1,17 +1,33 @@
 import React, { memo } from 'react'
+import { useIntl } from 'react-intl'
+import { formatIOMessage } from 'vtex.native-types'
+import { useCssHandles } from 'vtex.css-handles'
+import type { CssHandlesTypes } from 'vtex.css-handles'
 
-import NotificationContent from './components/NotificationContent'
+import { SanitizedHTML } from './components/SanitizedHTML'
+
+const CSS_HANDLES = ['notificationContent'] as const
 
 interface Props {
+  /** Text to be used in the bar */
   content?: string
+  /** Used to override default CSS handles */
+  classes?: CssHandlesTypes.CustomClasses<typeof CSS_HANDLES>
 }
 
-function NotificationInline({ content = '' }: Props) {
+function NotificationInline({ content = '', classes }: Props) {
+  const { handles } = useCssHandles(CSS_HANDLES, { classes })
+  const intl = useIntl()
+
   if (!content) {
     return null
   }
 
-  return <NotificationContent content={content} />
+  return (
+    <div className={handles.notificationContent}>
+      <SanitizedHTML content={formatIOMessage({ id: content, intl })} />
+    </div>
+  )
 }
 
 const MemoizedNotificationInline: React.MemoExoticComponent<
