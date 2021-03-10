dd7c3907f0741ba5a88990769e6613487644001e
Claudiu Ionita <claudiu@iviteb.com>
2021-03-10 12:57:17 +0200

    changed image exists check

--- react/components/ProductImages/utils/enums.js
+++ react/components/ProductImages/utils/enums.js
@@ -11,7 +11,7 @@ export const THUMBS_ORIENTATION = {
 export const DISPLAY_MODE = {
   CAROUSEL: 'carousel',
   LIST: 'list',
-  FIRST_IMAGE: 'first-image'
+  FIRST_IMAGE: 'first-image',
 }
 
 export const DEFAULT_EXCLUDE_IMAGE_WITH = 'skuvariation'
