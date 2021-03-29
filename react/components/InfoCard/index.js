b76f51c4e3341f250266340db4f36baa6ed4d285
jayendra88 <jayendra.k.a@gmail.com>
2021-03-29 13:28:55 +0530

    fix linting issue

--- react/components/InfoCard/index.js
+++ react/components/InfoCard/index.js
@@ -156,7 +156,7 @@ const InfoCard = ({
 
   const linkWrapperClasses = classNames(
     `${handles.infoCardImageLinkWrapper} no-underline`
-    )
+  )
 
   const textContainerClasses = classNames(
     `${handles.infoCardTextContainer} flex flex-column mw-100`,
