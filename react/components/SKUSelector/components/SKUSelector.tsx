04a92cf258efb8c5f4e4f781f7ca49bb22655825
Wender Lima <wender.lima@vtex.com.br>
2024-02-13 14:00:41 -0500

    Lint

--- react/components/SKUSelector/components/SKUSelector.tsx
+++ react/components/SKUSelector/components/SKUSelector.tsx
@@ -247,10 +247,10 @@ const variationNameToDisplayVariation =
     if (sortVariationsByLabel) {
       const allNumbers = options.every((option: any) => !isNaN(option.label))
       options.sort((a: any, b: any) => {
-          if (allNumbers) {
-              return a.label - b.label
-          }
-          return a.label < b.label ? -1 : a.label > b.label ? 1 : 0
+        if (allNumbers) {
+            return a.label - b.label
+        }
+        return a.label < b.label ? -1 : a.label > b.label ? 1 : 0
       })
     }
     return { name, originalName, options }
